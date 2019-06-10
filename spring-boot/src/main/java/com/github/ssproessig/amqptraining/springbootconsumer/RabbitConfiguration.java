ee3f7f6d520f4cf6e79ad71473ff0ebcbe4a2fcf
Sören Sprößig <ssproessig@gmail.com>
2019-06-10 15:54:10 +0200

    using configurable queue-name and binding-key

--- spring-boot/src/main/java/com/github/ssproessig/amqptraining/springbootconsumer/RabbitConfiguration.java
+++ spring-boot/src/main/java/com/github/ssproessig/amqptraining/springbootconsumer/RabbitConfiguration.java
@@ -1,6 +1,14 @@
 package com.github.ssproessig.amqptraining.springbootconsumer;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -13,4 +21,18 @@ public class RabbitConfiguration {
   @Value("${consumer-sample.binding-key:*.info}")
   private String bindingKey;
 
+  @Bean
+  public Queue queue() {
+    return QueueBuilder.durable(queueName).autoDelete().build();
+  }
+
+  @Bean
+  Exchange exchange() {
+    return ExchangeBuilder.topicExchange("amqp-training").durable(false).build();
+  }
+
+  @Bean
+  Binding binding(Queue consumerQueue, TopicExchange amqpTrainingExchange) {
+    return BindingBuilder.bind(consumerQueue).to(amqpTrainingExchange).with(bindingKey);
+  }
 }
--- spring-boot/src/main/java/com/github/ssproessig/amqptraining/springbootconsumer/SpringBootConsumerApplication.java
+++ spring-boot/src/main/java/com/github/ssproessig/amqptraining/springbootconsumer/SpringBootConsumerApplication.java
@@ -3,9 +3,6 @@ package com.github.ssproessig.amqptraining.springbootconsumer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,17 +18,7 @@ public class SpringBootConsumerApplication {
 
 
   @RabbitListener(
-      bindings = @QueueBinding(
-          value = @Queue(
-              name = "spring-boot-consumer"
-          ),
-          exchange = @Exchange(
-              name = "amqp-training",
-              type = "topic",
-              durable = "false"
-          ),
-          key = "*.info"
-      )
+      queues = "#{queue.name}"
   )
   public void onMessage(Message aMessage) {
     String payload = new String(aMessage.getBody());
